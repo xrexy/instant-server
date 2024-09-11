42953d2fd251cae929dde171c036de5fb5a4c50e
xrexy <rexydoesimportantthings@gmail.com>
2024-09-11 23:34:58 +0300

    feat: containerize app

--- api/server/routes/index.ts
+++ api/server/routes/index.ts
@@ -1,3 +1,10 @@
-export default eventHandler((event) => {
-  return "Start by editing <code>server/routes/index.ts</code>.";
+import { $ } from "bun";
+
+export default eventHandler(async () => {
+  const o1 = await $`ls`.text();
+  const o2 = await $`ls minecraft`.text();
+  const o21 = await $`cat ./minecraft/test.txt`.text();
+  await Bun.write("./minecraft/test.txt", Date.now().toString());
+  const o3 = await $`ls minecraft`.text();
+  return [o1, o2, o21, o3];
 });
